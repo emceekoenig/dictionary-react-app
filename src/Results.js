99d42363197facb41b324f12b1888fcfd23824be
emceekoenig <129642888+emceekoenig@users.noreply.github.com>
2023-08-07 16:40:20 -0500

    Created Sections

--- src/Results.js
+++ src/Results.js
@@ -7,20 +7,22 @@ export default function Results(props) {
   console.log(props.results);
   if (props.results) {
     return (
-      <div className="Results p-4">
-        <h4 className="Pronounciation text-muted  mb-0">
-          {props.results[0].hwi.prs[0].mw}
-        </h4>
+      <div className="Results">
+        <section>
+          <h4 className="Pronounciation text-muted  mb-0">
+            {props.results[0].hwi.prs[0].mw}
+          </h4>
 
-        <p className="AudioFile">{props.results[0].hwi.prs[0].sound.audio}</p>
+          <p className="AudioFile">{props.results[0].hwi.prs[0].sound.audio}</p>
 
-        {props.results.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
+          {props.results.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </section>
       </div>
     );
   } else {
